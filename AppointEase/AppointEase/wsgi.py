004dd8782a9554b6eb0b75b0660c760bf6253e35
dhruvilp8109 <dhruvilp8109@gmail.com>
2026-07-28 18:47:29 -0400

    Update wsgi.py

--- AppointEase/AppointEase/wsgi.py
+++ AppointEase/AppointEase/wsgi.py
@@ -1,13 +1,4 @@
 import os
-import sys
-from pathlib import Path
-
-# Fix path navigation: Move up past the inner folders to find 'store'
-CURRENT_DIR = Path(__file__).resolve().parent
-BASE_DIR = CURRENT_DIR.parent.parent
-sys.path.insert(0, str(BASE_DIR))
-sys.path.insert(0, str(CURRENT_DIR))
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppointEase.settings')
