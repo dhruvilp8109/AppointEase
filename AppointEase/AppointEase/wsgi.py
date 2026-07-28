090ec1e51038fbb48dd361ed0d314c965fb370df
dhruvilp8109 <dhruvilp8109@gmail.com>
2026-07-28 18:43:34 -0400

    Update wsgi.py

--- AppointEase/AppointEase/wsgi.py
+++ AppointEase/AppointEase/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for AppointEase project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
+
+# Fix path navigation: Move up past the inner folders to find 'store'
+CURRENT_DIR = Path(__file__).resolve().parent
+BASE_DIR = CURRENT_DIR.parent.parent
+sys.path.insert(0, str(BASE_DIR))
+sys.path.insert(0, str(CURRENT_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppointEase.AppointEase.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppointEase.settings')
 
 application = get_wsgi_application()
