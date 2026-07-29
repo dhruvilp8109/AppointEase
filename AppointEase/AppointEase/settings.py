08cf1baa747445a7e417da9863f8dc1a0418bf08
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2026-07-28 20:10:57 -0400

    testing database

--- AppointEase/AppointEase/settings.py
+++ AppointEase/AppointEase/settings.py
@@ -78,13 +78,35 @@ WSGI_APPLICATION = 'AppointEase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ae',
+#         'USER': 'root',
+#         'PASSWORD': 'Dna@182601',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTION': {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#             "charset": "utf8mb4"
+#         },
+#     }
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'TYLytcbuNZmLEMUCOmaSohAkCzqNYnjL',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
+        'OPTIONS': {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4"
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- AppointEase/store/models.py
+++ AppointEase/store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    class Meta:
+        db_table = 'ae_test'
+    test_id = models.BigAutoField(primary_key=True)
+    test_1 = models.CharField(max_length=255, null=True, blank=True)
+    test_2 = models.CharField(max_length=255, null=True, blank=True)
