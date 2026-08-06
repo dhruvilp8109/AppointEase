9e99d4134171083ba559dbe805ec78df3b840313
dhruvilp8109 <dhruvilp8109@gmail.com>
2026-08-05 22:05:06 -0400

    updated migrations

--- AppointEase/security/models.py
+++ AppointEase/security/models.py
@@ -5,7 +5,7 @@ from common.models import Audit
 
 class StoreAuthTokens(Audit):
     class Meta:
-        db_table = 'cx_store_auth_tokens'
+        db_table = 'ae_store_auth_tokens'
 
     access_token = models.TextField(null=True, db_column="auth_access_token")
     refresh_token = models.TextField(null=True, db_column="auth_refresh_token")
